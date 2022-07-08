3eb6a4bb682ab7799e08eda00a6259d1a37aeec0
JeFrC4 <jfrancoromero10@gmail.com>
2022-07-08 13:17:22 -0500

    jFranco

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*
+addEventListener('DOMContenteLoaded',() => {
+    const btn_menu = document.querySelector(".btn_menu");
+    if(btn_menu) {
+        btn_menu.addEventListener('click', () => {
+            const menu_items = document.querySelector(".menu_items");
+            menu_items.classList.toggle("ul.show")
+        })
+    }
+});
+*/
+
+
+window.onload = function() {  
+    var btn_menu = document.querySelector('.btn_menu') 
+    if (btn_menu) { 
+        btn_menu.addEventListener('click', () => { 
+            var menu_items = document.querySelector('.menu_items') 
+            menu_items.classList.toggle('show')
+        }) 
+    } 
+}
+
+
+
